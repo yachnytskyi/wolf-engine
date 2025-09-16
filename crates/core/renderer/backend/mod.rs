8ef8904d4ccbef7081b20a0ee91dc0a14720c1ca
yachnytskyi <yachnytskyi1992@gmail.com>
2025-09-16 16:31:20 +0300

    Refactoring

--- crates/core/renderer/backend/mod.rs
+++
@@ -1,6 +0,0 @@
-// #[cfg(feature = "vulkan")]
-// pub mod vulkan;
-
-// // Re-export the selected backend under a common name:
-// #[cfg(feature = "vulkan")]
-// pub use vulkan::VulkanRenderer as SelectedRenderer;
