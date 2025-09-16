de609d0292bc2112703d40be4f46c5ee8d00de7c
yachnytskyi <yachnytskyi1992@gmail.com>
2025-09-16 16:58:26 +0300

    Refactoring

--- engine/src/core.rs
+++ engine/src/core.rs
@@ -1,11 +1,5 @@
-pub mod renderer {
-    pub mod backend {
-        #[cfg(feature = "vulkan")]
-        pub use backend_vulkan::VulkanRenderer as SelectedRenderer;
+#[cfg(feature = "vulkan")]
+pub use backend_vulkan::VulkanRenderer as SelectedRenderer;
 
-        #[cfg(not(feature = "vulkan"))]
-        compile_error!("No renderer backend selected (enable the `vulkan` feature).");
-    }
-}
-
-pub use renderer::backend::SelectedRenderer;
+#[cfg(not(feature = "vulkan"))]
+compile_error!("No renderer backend selected (enable the `vulkan` feature).");
